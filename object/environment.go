504a5d19222a2ce47d72ff08659a94bfcffe2875
mochatek <akashsabu@ymail.com>
2022-10-21 19:39:19 +0530

    feat: Add environment(scope)

---
+++ object/environment.go
@@ -0,0 +1,37 @@
+package object
+
+type Environment struct {
+	store map[string]Object
+	outer *Environment
+}
+
+// Adds value to supplied identifier in the environment
+func (environment *Environment) Set(name string, object Object) Object {
+	environment.store[name] = object
+	return object
+}
+
+// Retrieves value of supplied identifier from environment
+// If identifier is not present in current environment, look up in outer environment (Scope chain)
+func (environment *Environment) Get(name string) (Object, bool) {
+	object, ok := environment.store[name]
+	if !ok && environment.outer != nil {
+		return environment.outer.Get(name)
+	}
+	return object, ok
+}
+
+// Constructor function for global environment
+// *outer points to null as this is the outermost environment
+func NewEnvironment() *Environment {
+	store := make(map[string]Object)
+	return &Environment{store: store, outer: nil}
+}
+
+// Constructor function for local environment
+// *outer points to the outer environment thereby creating the scope chain
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
