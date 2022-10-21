3ac29f55725546678478df19926dd7b1c065ada7
mochatek <akashsabu@ymail.com>
2022-10-21 19:34:27 +0530

    feat: Add token

---
+++ token/token.go
@@ -0,0 +1,96 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+// Identifiers and Literals
+const (
+	IDENTIFIER = "IDENTIFIER"
+	NUMBER     = "NUMBER"
+	TRUE       = "TRUE"
+	FALSE      = "FALSE"
+	STRING     = "STRING"
+)
+
+// Arithmetic Operators
+const (
+	PLUS     = "+"
+	MINUS    = "-"
+	ASTERISK = "*"
+	SLASH    = "/"
+	BANG     = "!"
+	ASSIGN   = "="
+)
+
+// Comparison Operators
+const (
+	EQ     = "=="
+	NOT_EQ = "!="
+	LT     = "<"
+	LT_EQ  = "<="
+	GT     = ">"
+	GT_EQ  = ">="
+)
+
+// Logical Operators
+const (
+	AND = "&"
+	OR  = "|"
+)
+
+// Parentheses, Braces and Special characters
+const (
+	L_PAREN   = "("
+	R_PAREN   = ")"
+	L_BRACE   = "{"
+	R_BRACE   = "}"
+	L_BRACKET = "["
+	R_BRACKET = "]"
+	COMMA     = ","
+	SEMICOLON = ";"
+	COLON     = ":"
+)
+
+// Keywords
+const (
+	LET      = "LET"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	FOR      = "FOR"
+	FUNCTION = "FUNCTION"
+	RETURN   = "RETURN"
+	IN       = "in"
+)
+
+// Others
+const (
+	EOF     = "EOF"
+	ILLEGAL = "ILLEGAL"
+)
+
+var Keywords = map[string]TokenType{
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"in":     IN,
+	"if":     IF,
+	"else":   ELSE,
+	"for":    FOR,
+	"fn":     FUNCTION,
+	"return": RETURN,
+}
+
+// Helper function to lookup a word in keyword dictionary
+// If word id present, then return its corresponding type
+// Else, return identifier type
+func LookUpKeywords(word string) TokenType {
+	if keywordType, ok := Keywords[word]; ok {
+		return keywordType
+	} else {
+		return IDENTIFIER
+	}
+}
